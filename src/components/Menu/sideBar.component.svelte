a5edccc9c3f298332415078cf69cb5289bf7bc17
Gustavo Giuliano <61397269+JinsuGiuliano@users.noreply.github.com>
2022-11-23 18:16:13 -0300

    added adapter to enable Netlify

--- src/components/Menu/sideBar.component.svelte
+++ src/components/Menu/sideBar.component.svelte
@@ -6,12 +6,16 @@ import sideBarState, { setSideBarState } from '../../stores/sideBarStore'
 
 <div class="drawer drawer-start max-w-xs">
   <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
-  <div class="drawer-content">
+  <div class="drawer-content w-100">
     <!-- Page content here -->
-    <label for="my-drawer-4" class="drawer-button btn glass w-auto">
+    <label for="my-drawer-4" class="drawer-button btn glass w-100 flex-center">
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 </svg>
+<span>NAVIGATE</span>
+  <div class="w-16 rounded">
+          <img src="/img/logo.png" />
+        </div>
     </label>
   </div> 
   <div class="drawer-side">
